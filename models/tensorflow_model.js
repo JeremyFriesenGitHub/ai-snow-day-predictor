11c4d0b69c054344862e38249e6c3e7fb5f29415
Jeremy Friesen <123791412+JeremyFriesenGitHub@users.noreply.github.com>
2023-12-12 21:24:22 -0500

    Update tensorflow_model.js

--- models/tensorflow_model.js
+++ models/tensorflow_model.js
@@ -1,22 +1,22 @@
-const tf = require('@tensorflow/tfjs-node');
-const loadData = require('./data_preparation');
+// const tf = require('@tensorflow/tfjs-node');
+// const loadData = require('./data_preparation');
 
-async function trainModel() {
-    const { trainFeatureTensor, trainLabelTensor, testFeatureTensor, testLabelTensor } = await loadData('corrected_dataset_3.csv');
+// async function trainModel() {
+//     const { trainFeatureTensor, trainLabelTensor, testFeatureTensor, testLabelTensor } = await loadData('corrected_dataset_3.csv');
 
-    const model = tf.sequential();
-    model.add(tf.layers.dense({ units: 50, activation: 'relu', inputShape: [6] }));
-    model.add(tf.layers.dense({ units: 30, activation: 'relu' }));
-    model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
+//     const model = tf.sequential();
+//     model.add(tf.layers.dense({ units: 50, activation: 'relu', inputShape: [6] }));
+//     model.add(tf.layers.dense({ units: 30, activation: 'relu' }));
+//     model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
 
-    model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy', metrics: ['accuracy'] });
+//     model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy', metrics: ['accuracy'] });
 
-    await model.fit(trainFeatureTensor, trainLabelTensor, {
-        epochs: 50,
-        validationData: [testFeatureTensor, testLabelTensor]
-    });
+//     await model.fit(trainFeatureTensor, trainLabelTensor, {
+//         epochs: 50,
+//         validationData: [testFeatureTensor, testLabelTensor]
+//     });
 
-    await model.save('file://models/tensorflow_model');
-}
+//     await model.save('file://models/tensorflow_model');
+// }
 
-trainModel();
+// trainModel();
