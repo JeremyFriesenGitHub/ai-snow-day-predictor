b193085f975051ab022a866735458d270c6a78f4
Jeremy Friesen <123791412+JeremyFriesenGitHub@users.noreply.github.com>
2023-12-12 21:22:30 -0500

    Update dashboardRoutes.js

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,24 +1,24 @@
-// //constants
-// const express = require('express');
-// const router = express.Router();
-// const db = require('../database'); // Ensure this points to your database module
+ //constants
+ const express = require('express');
+ const router = express.Router();
+ const db = require('../database'); // Ensure this points to your database module
 
-// router.post('/dashboard/get-prediction', (req, res) => {
-//   const { date } = req.body;
+ router.post('/dashboard/get-prediction', (req, res) => {
+   const { date } = req.body;
   
-//   // Check if the date exists in the database
-//   db.get("SELECT * FROM Historical_Data WHERE Date = ?", [date], (err, row) => {
-//     if (err) {
-//       res.status(500).send('Database error');
-//       return;
-//     }
+   // Check if the date exists in the database
+   db.get("SELECT * FROM Historical_Data WHERE Date = ?", [date], (err, row) => {
+     if (err) {
+       res.status(500).send('Database error');
+       return;
+     }
 
-//     // Pass both existingOutcome and dateForPrediction regardless of the condition
-//     res.render('dashboard', { 
-//       existingOutcome: row ? row.Outcome : undefined,
-//       dateForPrediction: date
-//     });
-//   });
-// });
+     // Pass both existingOutcome and dateForPrediction regardless of the condition
+     res.render('dashboard', { 
+       existingOutcome: row ? row.Outcome : undefined,
+       dateForPrediction: date
+     });
+   });
+ });
 
-// module.exports = router;
+ module.exports = router;
